c64032370e9a84bb090143492466687c643306f1
Sebastián Pérez Etchandy <sebastian@peretch.com>
2022-10-05 13:36:14 -0300

    Added routes example in controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,12 +3,17 @@ import { Controller, Get } from '@nestjs/common';
 // This decorator is telling nest that we are trying to create a class
 // that is gonna serve as a controller inside our application
 // This class is gonna handle and route incoming requests
-@Controller()
+@Controller('/app')
 export class AppController {
   // GET decorator allows us to create route handlers
   // to respond incoming requests that have the HTTP method of GET
-  @Get()
+  @Get('/hi')
   getRootRoute() {
     return 'Hi there!';
+  }
+  
+  @Get('/bye')
+  getByeThere() {
+    return 'bye there!';
   }
 }
